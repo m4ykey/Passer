325069055698ab0fbc3eafee07514be72a8c4926
kfjdj <kfjdjdkh@gmail.com>
2022-08-08 21:23:38 +0200

    finger print

---
+++ app/src/main/java/com/example/password_manager_room/FingerActivity.kt
@@ -0,0 +1,81 @@
+package com.example.password_manager_room
+
+import android.app.KeyguardManager
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CancellationSignal
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContentProviderCompat.requireContext
+
+class FingerActivity : AppCompatActivity() {
+
+    private var cancellationSignal : CancellationSignal? = null
+    private val authenticationCallback : BiometricPrompt.AuthenticationCallback
+        get() = @RequiresApi(Build.VERSION_CODES.P)
+        object : BiometricPrompt.AuthenticationCallback(){
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                super.onAuthenticationError(errorCode, errString)
+                Toast.makeText(this@FingerActivity, "Authentication error : $errString", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                super.onAuthenticationSucceeded(result)
+                startActivity(Intent(this@FingerActivity, MainActivity::class.java))
+            }
+        }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_finger)
+
+        checkBiometricSupport()
+
+        val btn : Button = findViewById(R.id.button)
+        btn.setOnClickListener {
+
+            val biometricPrompt = BiometricPrompt.Builder(this)
+                .setTitle("Wprowadź blokadę ekranu, aby uzyskać dostęp do aplikacji")
+                .setDescription("Odblokuj, aby kontynuować")
+                .setNegativeButton("Anuluj", this.mainExecutor) { _, _ ->
+                    Toast.makeText(this, "Authentication cancelled", Toast.LENGTH_SHORT).show()
+                }.build()
+
+            biometricPrompt.authenticate(getCancellationSignal(), mainExecutor, authenticationCallback)
+        }
+    }
+
+    private fun checkBiometricSupport() : Boolean {
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+
+        if (!keyguardManager.isKeyguardSecure){
+            Toast.makeText(this, "Fingerprint authentication has been not enabled in settings", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.USE_BIOMETRIC)!= PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(this, "Fingerprint authentication permission is not enabled", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return if (packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)){
+            true
+        }else true
+    }
+
+    private fun getCancellationSignal() : CancellationSignal {
+        cancellationSignal = CancellationSignal()
+        cancellationSignal?.setOnCancelListener {
+            Toast.makeText(this, "Authentication cancelled by the user", Toast.LENGTH_SHORT).show()
+        }
+        return cancellationSignal as CancellationSignal
+    }
+}
--- app/src/main/java/com/example/password_manager_room/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/password_manager_room/fragment/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.password_manager_room.fragment
 
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import android.os.Bundle
+import android.os.CancellationSignal
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
