09fd7c6225e2cf4174d51c0833690fb69699ba7c
kfjdj <kfjdjdkh@gmail.com>
2022-08-08 20:12:06 +0200

    repo mvvm

--- app/src/main/java/com/example/password_manager_room/mvvm/AccountViewModel.kt
+++ app/src/main/java/com/example/password_manager_room/mvvm/AccountViewModel.kt
@@ -1,4 +1,39 @@
 package com.example.password_manager_room.mvvm
 
-class AccountViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.room.Query
+import com.example.password_manager_room.data.Account
+import com.example.password_manager_room.repo.AccountRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class AccountViewModel(
+    private val accountRepository: AccountRepository
+) : ViewModel() {
+
+    val accounts = accountRepository.allAccount()
+    private val _searchAccount = MutableStateFlow<List<Account>>(emptyList())
+    val searchAccount : StateFlow<List<Account>> = _searchAccount
+
+    fun upsertAccount(account: Account){
+        viewModelScope.launch {
+            accountRepository.upsertNote(account)
+        }
+    }
+
+    fun deleteAccount(account: Account){
+        viewModelScope.launch {
+            accountRepository.deleteAccount(account)
+        }
+    }
+
+    fun searchInAccount(searchQuery: String){
+        viewModelScope.launch {
+            accountRepository.searchInAccount(searchQuery).collect{ accountList ->
+                _searchAccount.emit(accountList)
+            }
+        }
+    }
 }
