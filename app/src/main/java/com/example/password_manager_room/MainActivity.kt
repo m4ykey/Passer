879fc4a692f9f2863adf4fe2c128a2d32d2bbfd0
kfjdj <kfjdjdkh@gmail.com>
2022-08-08 20:44:34 +0200

    fragments

--- app/src/main/java/com/example/password_manager_room/MainActivity.kt
+++ app/src/main/java/com/example/password_manager_room/MainActivity.kt
@@ -2,8 +2,21 @@ package com.example.password_manager_room
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.password_manager_room.database.AccountDatabase
+import com.example.password_manager_room.mvvm.AccountViewModel
+import com.example.password_manager_room.mvvm.AccountViewModelProvider
+import com.example.password_manager_room.repo.AccountRepository
 
 class MainActivity : AppCompatActivity() {
+
+    val viewModel : AccountViewModel by lazy {
+        val database = AccountDatabase.getDatabase(this)
+        val accountRepository = AccountRepository(database)
+        val accountProviderFactory = AccountViewModelProvider(accountRepository)
+        ViewModelProvider(this, accountProviderFactory)[AccountViewModel::class.java]
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
