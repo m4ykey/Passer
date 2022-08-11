95f7a3b7aa93cc2059fc385d40549e4c5e29be2d
kfjdj <kfjdjdkh@gmail.com>
2022-08-11 20:04:23 +0200

    password generator

--- app/src/main/java/com/example/password_manager_room/MainActivity.kt
+++ app/src/main/java/com/example/password_manager_room/MainActivity.kt
@@ -3,10 +3,13 @@ package com.example.password_manager_room
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.password_manager_room.database.AccountDatabase
 import com.example.password_manager_room.mvvm.AccountViewModel
 import com.example.password_manager_room.mvvm.AccountViewModelProvider
 import com.example.password_manager_room.repo.AccountRepository
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -22,5 +25,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav : BottomNavigationView = findViewById(R.id.bottom_nav)
+        bottomNav.setupWithNavController(navController)
     }
 }
